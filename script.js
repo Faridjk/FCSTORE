c1068787f14f64d13657eb123300c9ac04a37d09
farid <farid.cafarov07.c@gmail.com>
2023-06-22 13:50:01 +0400

    Failure

---
+++ script.js
@@ -0,0 +1,29 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const firstCards = document.querySelectorAll('.firstcards1');
+    firstCards.forEach(item => {
+        item.addEventListener('touchstart', (event) => {
+            event.preventDefault();
+            const kartlar = document.querySelectorAll('.kartlar1');
+            kartlar.forEach(item => {
+                item.style.position = 'relative';
+                item.style.overflow = 'hidden';
+                item.style.zIndex = '2';
+            });
+            const overlay = document.querySelectorAll('.overlay2');
+            overlay.forEach(item => {
+                item.style.height = '100%';
+                item.style.width = '100%';
+                item.style.top = '0';
+                item.style.left = '0';
+                item.style.position = 'absolute';
+                item.style.display = 'flex';
+                item.style.alignItems = 'center';
+                item.style.justifyContent = 'center';
+                item.style.transition = '.4s';
+                item.style.opacity = '0';
+                item.style.color = 'white';
+                item.style.backgroundColor = ' rgba(35, 35, 37, 0.8)';
+            });
+        })
+    });
+});
